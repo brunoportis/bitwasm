96d82ddf57ac4597906c980b9e1ba19f3abadf08
Bruno Portis <bruno.porti@luizalabs.com>
2024-09-27 18:55:22 -0300

    feat: base

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,102 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashMap;
+
+use wasm_bindgen::prelude::*;
+use serde::{Serialize, Deserialize};
+use console_error_panic_hook;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Serialize, Deserialize)]
+#[wasm_bindgen]
+pub struct BitmapIndex {
+    index: HashMap<String, Vec<u32>>
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[wasm_bindgen]
+impl BitmapIndex {
+
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> BitmapIndex {
+        BitmapIndex {
+            index: HashMap::new()
+        }
+    }
+
+    #[wasm_bindgen]
+    pub fn insert(&mut self, key: String, id: u32) {
+        let entry = self.index.entry(key).or_insert(Vec::new());
+
+        if id as usize / 32 >= entry.len() {
+            entry.resize(id as usize / 32 + 1, 0);
+        }
+
+        entry[id as usize / 32] |= 1 << (id % 32);
+    }
+
+    #[wasm_bindgen]
+    pub fn get(&self, key: &str, id: usize) -> bool {
+        if let Some(entry) = self.index.get(key) {
+            let word_index =  id / 32;
+            if word_index < entry.len() {
+
+                // aqui estamos criando uma máscara com um único bit setado para 1
+                // ex. se id = 10, entao (1 << (10 % 32)) = (1 << 10) = 2^10 = 1024 -> 0000 0000 0000 0000 0000 0100 0000 0000
+                let mask = 1 << (id % 32);
+
+                // aqui estamos pegando o valor da word no índice word_index
+                let value = entry[word_index];
+
+                // aqui estamos usando o operador AND bit a bit para verificar se o bit está setado para 1
+                let masked_value = value & mask;
+
+                // true se o bit estiver setado para 1
+                return masked_value != 0;
+            }
+        }
+        false
+    }
+
+    #[wasm_bindgen]
+    pub fn list(&self, key: &str) -> Vec<u32> {
+        if let Some(entry) = self.index.get(key) {
+            let mut result = Vec::new();
+            for (i, word) in entry.iter().enumerate() {
+                let mut mask = 1;
+                for _ in 0..32 {
+                    if word & mask != 0 {
+                        result.push(i as u32 * 32 + mask.trailing_zeros());
+                    }
+                    mask <<= 1;
+                }
+            }
+            return result;
+        }
+        Vec::new()
+    }
+
+    #[wasm_bindgen]
+    pub fn batch_insert(&mut self, key: String, ids: Vec<u32>) {
+        for id in ids {
+            self.insert(key.clone(), id);
+        }
     }
 }
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+
+    let mut index = BitmapIndex::new();
+
+    index.batch_insert("tem_pendencias".to_string(), [1, 3, 5, 7, 9].to_vec());
+    index.batch_insert("is_admin".to_string(), [256, 512, 1024].to_vec());
+
+    log(&format!("Com pendencias: {:?}", index.list("tem_pendencias")));
+
+    log(&format!("512 é admin?: {:?}", index.get("is_admin", 512)));
+}
+ 
